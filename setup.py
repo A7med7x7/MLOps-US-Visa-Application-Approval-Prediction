c29960cdfbc90a1539012f24906d2106d5540d23
ahmed alghali <a7medalghali777@gmail.com>
2024-08-07 12:59:43 +0300

    enviroment setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Ahmed_Alghali",
+    author_email="a7medalghali777@gmail.com",
+    packages=find_packages()
+)
